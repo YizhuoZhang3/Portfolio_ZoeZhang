f44f4dba58344b05d647b2753bbaa00ade75cd2e
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-12-19 00:43:50 -0500

    styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,22 +138,22 @@ $('.grid-item', workWrapper).reCalWidth();
 }
 work();
 
-$('.portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: true,
-    fixedContentPos: true,
-    closeMarkup: '<button title="%title%" type="button" class="mfp-close"><i class="pe-7s-close"></i></button>',
+// $('.portfolio').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     closeOnContentClick: true,
+//     closeBtnInside: true,
+//     fixedContentPos: true,
+//     closeMarkup: '<button title="%title%" type="button" class="mfp-close"><i class="pe-7s-close"></i></button>',
 
-removalDelay: 500, //delay removal by X to allow out-animation
-    callbacks: {
-        beforeOpen: function() {
-            this.st.mainClass = this.st.el.attr('data-effect');
-        }
-    },
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-});
+// removalDelay: 500, //delay removal by X to allow out-animation
+//     callbacks: {
+//         beforeOpen: function() {
+//             this.st.mainClass = this.st.el.attr('data-effect');
+//         }
+//     },
+//     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+// });
 
 $('.portfolio .popup-video').magnificPopup({
     disableOn: 700,
